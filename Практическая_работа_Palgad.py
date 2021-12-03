96fd0c32c00344e4e89b38b866feda17b29ccfb4
opilane <opilane@HP-CZC6297B82>
2021-12-03 11:58:36 +0200

    palgad

--- Практическая_работа_Palgad.py
+++ Практическая_работа_Palgad.py
@@ -1,24 +1,24 @@
 from random import *
 inimised=["Pillipets", "Zemlyakov", "Vinogradova", "Komarova", "Kerve", "Illing"]
-palgad=[2500, 2300, 2000, 1900, 1700, 1500, 1300, 500]
+palgad=[2500, 2300, 2000, 1900, 1700, 1500]
 
 def sisesta_andmed(i,p): #Добавить в список новых три фамилии
     N=3
-    for n in range(N):
-        a=input("Введите фамилию нового работника: ")
-        i.append(a)
+    for n in range(N): #Ставит рандомному работнику рандомную зарплату
+        a=input("Введите фамилию нового работника: ") #просит ввести
+        i.append(a) #Добавляет элемент в конец списка 
         palk=randint(100,10000) #Вводит рандомную зарплату новому работнику
-        p.append(palk)
+        p.append(palk) #Добавляет элемент в конец списка 
     return i,p
 
 def andmed_ekranile(i,p): #выводит на экран список имен и зарплат
-    N=len(i)
-    for n in range(N):
+    N=len(i) #Возвращает число
+    for n in range(N): #Ставит рандомному работнику рандомную зарплату
         print(i[n],"-",p[n])
 
 def kustutamine(i,p): #удаляет человека из списка
-    nimi=input("Введите имя для удаления из списка: ")
-    n=i.count(nimi)
+    nimi=input("Введите имя для удаления из списка: ") #просит ввести
+    n=i.count(nimi) #Возвращает количество элементов со значением nimi
     abi_list=[]
     if n>0:
         t=0
@@ -27,26 +27,26 @@ def kustutamine(i,p): #удаляет человека из списка
                 t+=1
                 abi_list.append(int(e)) #список индексов
                 print(t,". ",i[e],"-", p[e])
-        jaar=int(input("Порядковый номер: "))
-        i.pop(abi_list[jaar-1])
-        p.pop(abi_list[jaar-1])
+        jaar=int(input("Порядковый номер: ")) #просит ввести
+        i.pop(abi_list[jaar-1]) #Удаляет i-ый элемент и возвращает его
+        p.pop(abi_list[jaar-1]) #Удаляет i-ый элемент и возвращает его
         andmed_ekranile(i,p)
     return i,p
         
 def top3(i,p,v): #три самых высоких зарплаты
     if v==1:
-        p.sort()
-        p.reverse()
-        i.sort()
-        i.reverse()
+        p.sort() #Сортирует список 
+        p.reverse() #Разворачивает список 
+        i.sort() #Сортирует список 
+        i.reverse() #Разворачивает список 
         for h in range (0,len(p),1):
             if h>=3:
                 break
             print(p[h])
             print(i[h])
     elif v==2:
-        p.sort()
-        i.sort()
+        p.sort() #Сортирует список 
+        i.sort() #Сортирует список 
         for j in range(0,len(p),1):
             if j>=3:
                 break
@@ -55,12 +55,12 @@ def top3(i,p,v): #три самых высоких зарплаты
 
 def keskmine_kustutamine(i,p): #удаляет все зарплаты ниже средней
     summa=0
-    new_list5=[]
+    new_list5=[] #Делает новый список
     for palk in p:
         summa+=palk
     summa/=len(p)
     print("Средняя зарплата составляет - ", summa, " все зарплаты ниже этой цифры будут удалены!")
-    w=0
+    w=0 #w припавнивается к нулю
     while w<len(p):
         if p[w]<summa:
             del p[w]
@@ -70,8 +70,8 @@ def keskmine_kustutamine(i,p): #удаляет все зарплаты ниже 
 
 
 def name_search1(i,p):
-    nimi=input("Введите имя работника: ")
-    n=i.count(nimi)
+    nimi=input("Введите имя работника: ") #просит ввести 
+    n=i.count(nimi) #Возвращает количество элементов со значением nimi
     if n>0:
         t=0
         for e in range(len(i)):
